70c9e2ecfb5a99064b6e7e13f775bdcc48be7c93
Ammar <ammaradlan30@gmail.com>
2023-01-07 14:02:23 -0500

    edit

--- passport/settings.py
+++ passport/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*4@x!e99@5xvgcb5pv5l0-#d=dit-5fl14df$xm3mybbqb(q2+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
